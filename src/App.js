4e9fda31536e2f3cdf75fabc458fe5be385c0d54
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-28 00:11:54 -0700

    prints pokemon objects to page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
 import './App.css';
+// import Example from './components/Example';
+import PokemonAPI from './components/PokemonAPI';
 
 function App() {
   return (
     <div className="App">
       <h1>check one two</h1>
       <h2>is this thing on?</h2>
+      {/* <Example /> */}
+      <PokemonAPI />
     </div>
   );
 }
--- src/components/PokemonAPI.js
+++ src/components/PokemonAPI.js
@@ -2,6 +2,14 @@ import React, {useEffect, useState} from 'react';
 import '../App.css';
 
 const PokemonAPI = (props) => {
+  const [pokemon, setPokemon] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon')
+      .then(response => response.json())
+      .then(response => setPokemon(response.results))
+  },[]);
+    console.log(pokemon);
   return(
     <>
     <div>This is a message from the PokemonAPI component</div>
