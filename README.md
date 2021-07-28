251be6c0c852a7829da884326f835058baa435e6
Sevrinn <sevrinnsevrinn@gmail.com>
2021-07-27 21:24:47 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Poke API
+
+Created a React project that uses the useEffect hook to display the names of all the pokemon with the Pokemon API.
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>check one two</h1>
+      <h2>is this thing on?</h2>
+    </div>
+  );
+}
+
+export default App;
